dfe1c8c6ada9954e15538b58a8d0b739b86cf5a9
burritoscott <36803614+burritoscott@users.noreply.github.com>
2018-06-03 21:02:44 -0700

    input some notes into the js file for later use

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//create an array for the alphabet.
+//use 'onkeyup' to trigger a for loop to loop through the alphabet array.
+//find out how to remove letter from the array when it is guessed
+//find out how to apply that letter to the word when it is guessed correctly
+
+//Theme will be Nintendo
+//create three things to guess:  The Legend of Zelda,  Metroid, Mario Bros
+//find sound bites for guesses and wins/loses
